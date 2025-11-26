976e5966067ba9d330e00e0160118695cd7760a8
Andrii Shapovalov <work.shapovalov.andrew@gmail.com>
2025-11-26 20:20:34 +0200

    replace ‘ffill’ with ‘linear’ to theoretically avoid combing

--- TSA_L1/data_handler.py
+++ TSA_L1/data_handler.py
@@ -5,5 +5,5 @@ def prepare_timeseries(df: pd.DataFrame) -> pd.DataFrame:
     df['timestamp'] = pd.to_datetime(df['timestamp'])
     df = df.set_index('timestamp')
 
-    df_resampled = df.resample('1h').last().ffill().fillna(0)
+    df_resampled = df.resample('1h').last().interpolate(method='linear').fillna(0)
     return df_resampled
