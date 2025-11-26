ae8af83431f7833351710838c59331c7974990b2
Andrii Shapovalov <work.shapovalov.andrew@gmail.com>
2025-11-26 21:14:17 +0200

    add sorting timestamps for input data

--- TSA_L1/data_handler.py
+++ TSA_L1/data_handler.py
@@ -4,6 +4,8 @@ def prepare_timeseries(df: pd.DataFrame) -> pd.DataFrame:
     
     df['timestamp'] = pd.to_datetime(df['timestamp'], format='%d.%m.%Y %H:%M:%S')
     df = df.set_index('timestamp')
+    
+    df = df.sort_index()
 
     df_resampled = df.resample('1h').last().interpolate(method='linear').fillna(0)
     return df_resampled
